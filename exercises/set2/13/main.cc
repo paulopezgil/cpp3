cde63185e9ae7a495d4a0cddc9a3318369267148
paulopezgil <plopezgi53@alumnes.ub.edu>
2025-02-20 13:26:25 +0100

    set2 completed

--- exercises/set2/13/main.cc
+++ exercises/set2/13/main.cc
@@ -6,7 +6,9 @@ using namespace std;
 
 int main()
 {
-    // use copy generic algorithm to fill a Data object with lines from cin.
     Data obj;
+
+    // use copy generic algorithm to fill a Data object with lines from cin.
     copy(istream_iterator<string>(cin), istream_iterator<string>(), 
+                                                        back_inserter(obj));
 }
