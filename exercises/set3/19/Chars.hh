a142e08142ca1fc258bb99f8eeb94db698a99870
paulopezgil <plopezgi53@alumnes.ub.edu>
2025-02-26 14:01:09 +0100

    ex19

---
+++ exercises/set3/19/Chars.hh
@@ -0,0 +1,24 @@
+#ifndef CHARS_HH
+#define CHARS_HH
+
+#include <tuple>
+#include <ostream>
+
+template <char ...Args>
+class Chars
+{
+    // allow insertions into ostreams
+    template <char ...Params>
+    friend std::ostream& operator<<(std::ostream& os, const Chars<Params...>&);
+    //friend std::ostream &operator<<(std::ostream &out, Chars const &chars);
+};
+
+template <char ...Args>
+inline std::ostream &operator<<(std::ostream &out, Chars<Args...> const &chars)
+{
+    // Use unary left fold to insert each character into the stream
+    return (out << ... << Args);
+}
+
+
+#endif
