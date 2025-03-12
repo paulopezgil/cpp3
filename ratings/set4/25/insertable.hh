2131586254501cb2ee041bfa868a84d2adb76072
paulopezgil <plopezgi53@alumnes.ub.edu>
2025-03-12 17:28:18 +0100

    ratings uploaded

---
+++ ratings/set4/25/insertable.hh
@@ -0,0 +1,28 @@
+// JB: ?
+#ifndef INSERTABLE_HH
+#define INSERTABLE_HH
+
+
+template <template <typename> class Container, typename Type>
+struct Insertable : public Container<Type>
+{
+    // default, copy and move constructors
+    Insertable() = default;
+    Insertable(Insertable const &other) = default;
+    Insertable(Insertable &&tmp) = default;
+
+    // initialization by an existing data storage object
+    Insertable(Container<Type> const &other); //JB: SF <type>.
+    Insertable(Container<Type> &&tmp);
+
+    // initialization by a single data element of the used container
+    Insertable(Type const &other);
+    Insertable(Type &&tmp);
+};
+
+// #include "insertable.i"
+// the file is provided in ex 27, therefore outcommented here.
+//JB: While that's fair enough, the exercise calls for a class that is
+//JB: insertable into an ostream. This one is not.
+
+#endif
