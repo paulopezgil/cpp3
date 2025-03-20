c84916fe42801878b9148db97c86c4d6338ccddd
paulopezgil <plopezgi53@alumnes.ub.edu>
2025-03-20 14:33:52 +0100

    48 works

--- exercises/set6/48/main.cc
+++ exercises/set6/48/main.cc
@@ -1,8 +1,23 @@
 #include "scanner/Scanner.h"
+#include <exception>
+#include <iostream>
+using namespace std;
 
-int main()
+int main(int argc, char *argv[])
+try
 {
-    Scanner scanner;
+    // a file name must be provided
+    if (argc != 2)
+        throw std::runtime_error("Please specify a file name\n");
 
+    // initialize the scanner
+    Scanner scanner;
+    scanner.switchIstream(argv[1]);
+
+    // read the input file
     scanner.lex();
+}
+catch(std::runtime_error exc)
+{
+    cerr << "Program ended after catching an exception: " << exc.what();
 }
---
+++ exercises/set6/48/scanner/included.cc
@@ -0,0 +1,4 @@
+#include "Scanner.h"
+
+// definition of d_included member
+std::set<std::string> Scanner::d_included;
