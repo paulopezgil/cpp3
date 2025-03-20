e42c3ae00a3ac03d24e3d23e39d43f9fdfd960d8
paulopezgil <plopezgi53@alumnes.ub.edu>
2025-03-20 12:38:41 +0100

    raw string literal

--- exercises/set6/43/inp.cc
+++
@@ -1,4 +0,0 @@
-kgowineoi qwe wepwgq 
-owiefowebgob // qeufweufbi "iwenwefbubfw"
-wefon ew fo e grabbed(1, "inp.cc") we fwe f grabbed(2, "inp.cc")
-gjwe oewifn ow grabbed(3, "inp.cc")
--- exercises/set6/43/out.cc
+++
@@ -1,3 +0,0 @@
-kgowineoi qwe wepwgq 
-wefon ew fo e grabbed(1, "(istream)") we fwe f grabbed(2, "(istream)")
-gjwe oewifn ow grabbed(3, "(istream)")
