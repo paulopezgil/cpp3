0e154209f720805e7fe2c64ac120694c0fe322fe
paulopezgil <plopezgi53@alumnes.ub.edu>
2025-03-20 12:28:36 +0100

    43 works

---
+++ exercises/set6/43/inp.cc
@@ -0,0 +1,3 @@
+kgowineoi qwe wepwgq 
+wefon ew fo e grabbed(1, "inp.cc") we fwe f grabbed(2, "inp.cc")
+gjwe oewifn ow grabbed(3, "inp.cc")
---
+++ exercises/set6/43/out.cc
@@ -0,0 +1,3 @@
+kgowineoi qwe wepwgq 
+wefon ew fo e grabbed(1, "(istream)") we fwe f grabbed(2, "(istream)")
+gjwe oewifn ow grabbed(3, "(istream)")
